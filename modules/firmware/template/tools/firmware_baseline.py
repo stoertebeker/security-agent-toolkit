2a18b144b2b54678b7a2875a7100f788c88149e8
stoertebeker <schramm.norbert@mailbox.org>
2026-08-22 22:59:09 +0200

    Run firmware baseline enrichment

--- modules/firmware/template/tools/firmware_baseline.py
+++ modules/firmware/template/tools/firmware_baseline.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-from firmware_baseline_core import main
+from firmware_baseline_core import main as baseline_main
+from firmware_baseline_enrich import main as enrich_main
 
 if __name__ == "__main__":
-    raise SystemExit(main())
+    rc = baseline_main()
+    if rc:
+        raise SystemExit(rc)
+    raise SystemExit(enrich_main())
