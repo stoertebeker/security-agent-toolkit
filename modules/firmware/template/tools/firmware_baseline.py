2dac04404fccfd8d6e31d7fba1b8360692086637
stoertebeker <schramm.norbert@mailbox.org>
2026-08-26 21:44:33 +0200

    Run firmware identity extraction in baseline stage

--- modules/firmware/template/tools/firmware_baseline.py
+++ modules/firmware/template/tools/firmware_baseline.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+from firmware_identity import main as identity_main
 from firmware_baseline_core import main as baseline_main
 from firmware_baseline_enrich import main as enrich_main
 from firmware_web_surface import main as web_surface_main
 
 if __name__ == "__main__":
+    rc = identity_main()
+    if rc:
+        raise SystemExit(rc)
     rc = baseline_main()
     if rc:
         raise SystemExit(rc)
